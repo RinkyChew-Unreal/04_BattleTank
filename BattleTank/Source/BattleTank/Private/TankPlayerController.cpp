4515862d91e121ff8e91caf801c363db095fa127
DESKTOP-AT9GER2\mmcel <mmcelt@yahoo.com>
2017-12-22 20:19:08 -0700

    122 - 2 Using virtual & override

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -9,3 +9,18 @@ ATank* ATankPlayerController::GetControlledTank() const
 {
    return Cast<ATank>(GetPawn());
 }
+
+void ATankPlayerController::BeginPlay()
+{
+   Super::BeginPlay();
+   auto MyTank = GetControlledTank();
+
+   if (!MyTank) 
+   {
+      UE_LOG(LogTemp, Warning, TEXT("TankPlayerController: I Don't Control a Tank!"));
+   }
+   else
+   {
+      UE_LOG(LogTemp, Warning, TEXT("TankPlayerController: I Control Tank: %s"), *MyTank->GetName());
+   }
+}
