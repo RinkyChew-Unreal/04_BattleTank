84531735bf9896ab86818ef7bc6d7ea2ac89b9c2
DESKTOP-AT9GER2\mmcel <mmcelt@yahoo.com>
2017-12-24 13:14:12 -0700

    133 - Creating Default Sub Objects in C++

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -10,6 +10,8 @@ ATank::ATank()
    // Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
    PrimaryActorTick.bCanEverTick = true;
 
+   //no need to protect pointers as added at construction
+   TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
 }
 
 // Called when the game starts or when spawned
@@ -34,8 +36,7 @@ void ATank::SetupPlayerInputComponent(class UInputComponent* InputComponent)
 
 void ATank::AimAt(FVector HitLocation)
 {
-   auto MyTankName = GetName();
-   UE_LOG(LogTemp, Warning, TEXT("%s Aiming at: %s"), *MyTankName, *HitLocation.ToString());
+   TankAimingComponent->AimAt(HitLocation);
 }
 
 
--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
 
@@ -11,21 +12,21 @@ class BATTLETANK_API ATank : public APawn
    GENERATED_BODY()
 
 public:
+
+   void AimAt(FVector HitLocation);
+
+protected:
+   UTankAimingComponent* TankAimingComponent = nullptr;
+private:
    // Sets default values for this pawn's properties
    ATank();
 
    // Called when the game starts or when spawned
    virtual void BeginPlay() override;
-   
+
    // Called every frame
-   virtual void Tick( float DeltaSeconds ) override;
+   virtual void Tick(float DeltaSeconds) override;
 
-   void AimAt(FVector HitLocation);
-
-private:
    // Called to bind functionality to input
    virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
-
-   
-   
 };
