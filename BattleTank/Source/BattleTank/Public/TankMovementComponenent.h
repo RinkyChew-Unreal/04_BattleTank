17fb4fdc34d497c6750d70a8914b8c1b36e5cce2
DESKTOP-AT9GER2\mmcel <mmcelt@yahoo.com>
2017-12-30 15:49:04 -0700

    158 - Fly-by-Wire Control System

---
+++ BattleTank/Source/BattleTank/Public/TankMovementComponenent.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/NavMovementComponent.h"
+#include "TankMovementComponenent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API UTankMovementComponenent : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
