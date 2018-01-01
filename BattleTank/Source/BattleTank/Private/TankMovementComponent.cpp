388f438e8c6cf04dd8383366e938bcce60020ed3
DESKTOP-AT9GER2\mmcel <mmcelt@yahoo.com>
2018-01-01 15:53:06 -0700

    166 - Finalizing Your Class Code

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright RinkyChew LLC
 
 #include "BattleTank.h"
 #include "TankTrack.h"
@@ -15,8 +15,6 @@ void UTankMovementComponent::IntendMoveForward(float Throw)
    if (!LeftTrack || !RightTrack) { return; }
    LeftTrack->SetThrottle(Throw);
    RightTrack->SetThrottle(Throw);
-
-   //TODO prevent double-speed due to dual control use
 }
 
 void UTankMovementComponent::IntendTurnRight(float Throw)
@@ -24,8 +22,6 @@ void UTankMovementComponent::IntendTurnRight(float Throw)
    if (!LeftTrack || !RightTrack) { return; }
    LeftTrack->SetThrottle(Throw);
    RightTrack->SetThrottle(-Throw);
-
-   //TODO prevent double-speed due to dual control use
 }
 
 void UTankMovementComponent::RequestDirectMove(const FVector & MoveVelocity, bool bForceMaxSpeed)
@@ -39,8 +35,5 @@ void UTankMovementComponent::RequestDirectMove(const FVector & MoveVelocity, boo
 
    auto RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
    IntendTurnRight(RightThrow);
-
-   //auto Name = GetOwner()->GetName();
-   //UE_LOG(LogTemp, Warning, TEXT("%s Vectoring to: %s"), *Name, *AIForwardIntention.ToString());
 }
 
--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright RinkyChew LLC
 
 #pragma once
 
@@ -18,21 +18,19 @@ class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
    
    
 public:   
-   UFUNCTION(BlueprintCallable, Category = Setup)
+   UFUNCTION(BlueprintCallable, Category = "Setup")
    void Initialize(UTankTrack * LeftTrackToSet, UTankTrack * RightTrackToSet);
 
-   UFUNCTION(BlueprintCallable, Category=Input)
+   UFUNCTION(BlueprintCallable, Category="Input")
    void IntendMoveForward(float Throw);
 
-   UFUNCTION(BlueprintCallable, Category = Input)
+   UFUNCTION(BlueprintCallable, Category = "Input")
    void IntendTurnRight(float Throw);
-
-   //TODO check best protection level
-   /** path following: request new velocity */
-   virtual void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override;
 
 private:
    UTankTrack* LeftTrack = nullptr;
    UTankTrack* RightTrack = nullptr;
 
+   //called from the pathfinding logic by the AI Controllers
+   virtual void RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) override;
 };
