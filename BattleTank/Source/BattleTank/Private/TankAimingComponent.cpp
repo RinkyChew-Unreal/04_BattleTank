3ce6bc2c4a0a7c0e067389854ba0bb2f7af56ebf
DESKTOP-AT9GER2\mmcel <mmcelt@yahoo.com>
2018-01-03 15:58:54 -0700

    180 - EOL cleanup

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -12,7 +12,7 @@ UTankAimingComponent::UTankAimingComponent()
 {
    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
    // off to improve performance if you don't need them.
-   bWantsBeginPlay = true;
+   //bWantsBeginPlay = true;
    PrimaryComponentTick.bCanEverTick = true;
 }
 
@@ -28,7 +28,6 @@ void UTankAimingComponent::TickComponent(float DeltaTime, enum ELevelTick TickTy
    {
       FiringState = EFiringState::Reloading;
    }
-   UE_LOG(LogTemp, Warning, TEXT("I'm Ticking!"))
    // TODO Handle aiming and locked states
 }
 
